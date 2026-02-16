fe88078b9b7ec89459cbff8a977b0e9fd445b30e
premprakash yadav <84430639+premprakash8080@users.noreply.github.com>
2026-02-16 16:28:46 +0530

    Fixed

--- src/routes/campaign.routes.js
+++ src/routes/campaign.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const CampaignController = require("../controllers/campaign.controller");
-const { protect } = require("../config/jwtVerify");
+const jwtController = require("../config/jwtVerify");
 
 const { getCampaigns } = CampaignController();
 
-router.use(protect);
+router.use(jwtController.protect);
 router.get("/get-campaigns", getCampaigns);
 
 module.exports = router;
--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const OrderController = require("../controllers/order.controller");
-const { protect } = require("../config/jwtVerify");
+const jwtController = require("../config/jwtVerify");
 
 const { getOrders } = OrderController();
 
-router.use(protect);
+router.use(jwtController.protect);
 router.get("/get-orders", getOrders);
 
 module.exports = router;
